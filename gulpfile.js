3a7d16a68d08968a0e3a6b1bf67bb9e09c4c8c6c
Andras Simon <andras.gearbox@gmail.com>
2017-01-19 09:44:44 +0100

    Latest updates to website

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,9 @@
-var gulp = require('gulp'),
-watch = require('gulp-watch'),
-postcss = require('gulp-postcss'),
-autoprefixer  = require('autoprefixer'),
-cssvars = require('postcss-simple-vars');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
-gulp.task('default', function(){
-console.log("Hooray - you created a GULP task!");
 
-});
 
-gulp.task('html', function(){
-console.log("Imagine something getting done to your HTML");
-});
 
-gulp.task('styles', function(){
-return gulp.src('./app/assets/styles/styles.css')
-	.pipe(postcss([autoprefixer, cssvars]))
-	.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function(){
-watch('./app/index.html', function(){
-gulp.start('html');	
-})
 
-watch('./app/assets/styles/**/*.css', function(){
-gulp.start('styles');	
-});
 
-});
