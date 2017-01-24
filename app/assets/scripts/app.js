4eb58abe4f1ca24d5b5cb325d46b7cd3eca14977
Andras <andras.gearbox@gmail.com>
2017-01-24 17:40:23 +0100

    Finished implementing the Reveal on scroll functions.

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,3 +1,10 @@
+import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/reveal-on-scroll';
+
 
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"),"85%");
+new RevealOnScroll($(".testimonial"),"60%");
+
+
