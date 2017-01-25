d3b9f7f623aab1b2f6b1c364e05a1bf763c861de
Andras <andras.gearbox@gmail.com>
2017-01-25 22:07:46 +0100

    Finished the MODAL overlay

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -3,10 +3,12 @@ import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/reveal-on-scroll';
 import StickyHeader from './modules/sticky-header';
+import Modal from'./modules/modal';
 
 
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"),"85%");
 new RevealOnScroll($(".testimonial"),"60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
 
